0d0f90eee9413e58499919fd5a255c289bd14e2d
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 02:52:20 +0300

    Update ListTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/ListTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/ListTranslator.java
@@ -3,8 +3,6 @@ import gen.*;
 import Translation.TranslateAndError.Translator;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-
-// formalsList: formalsList COMMA formal | formal;
 public class ListTranslator extends Translator {
     ParseTree parseTree ;
     public ListTranslator(ParseTree parsetree) {
