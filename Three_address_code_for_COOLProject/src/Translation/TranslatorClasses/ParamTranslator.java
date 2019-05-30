93189c9ec860e1ae1ddd31bc42694fd19685a234
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 11:37:53 +0300

    Update ParamTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/ParamTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/ParamTranslator.java
@@ -3,20 +3,26 @@ import gen.*;
 import Translation.TranslateAndError.Translator;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+//param',' formal|formal
 public class ParamTranslator extends Translator {
     ParseTree parseTree ;
+    //ensure that the child of parsetree is param
     public ParamTranslator(ParseTree parsetree) {
         super(parsetree, COOLParser.ParamContext.class);
         parseTree = parsetree;
     }
 
+    // generate 3address code for decl
     @Override
     public String generate() {
+        //check that it has one child
         if(parseTree.getChildCount() == 1){
 
             new FormalTranslator(parseTree.getChild(0)).generate();
 
-        }else{
+        }
+        //has 3 children
+        else{
 
             new ParamTranslator(parseTree.getChild(0)).generate();
 
