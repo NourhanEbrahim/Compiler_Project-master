ccb7793d6b19f56d62deef7180bfbb9340bc3180
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 11:33:38 +0300

    Update NegativeTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/NegativeTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/NegativeTranslator.java
@@ -7,17 +7,21 @@ import Translation.TranslateAndError.Translator;
 import gen.COOLParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+//INTEGER_NEGATIVE stmt
 public class NegativeTranslator extends Translator {
     ParseTree parseTree;
+    //ensure that child of expression is Negative
     public NegativeTranslator(ParseTree parsetree) {
         super(parsetree, COOLParser.NegativeExprContext.class);
         parseTree =parsetree;
     }
 
+    //generate 3address code for negative
     @Override
     public String generate() {
         String childTemp = new stmtTranslator(parseTree.getChild(1)).generate();
         Temp temp = new Temp();
+        // assign expression's value into variable
         TranslationHandler.write(space + temp.toString() + " = ~ " + childTemp.toString());
         return temp.toString();
     }
