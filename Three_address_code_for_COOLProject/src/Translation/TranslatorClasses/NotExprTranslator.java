0d347960dfc6ee7155235a5c2ec4503db70fb48e
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 11:35:41 +0300

    Update NotExprTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/NotExprTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/NotExprTranslator.java
@@ -7,17 +7,21 @@ import Translation.TranslateAndError.Translator;
 import gen.COOLParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+//NOT stmt
 public class NotExprTranslator extends Translator {
     ParseTree parseTree;
+    //ensure that the child of parsetree is Notexpr
     public NotExprTranslator(ParseTree parsetree) {
         super(parsetree, COOLParser.NotExprContext.class);
         parseTree =parsetree;
     }
 
+    // generate 3address code for Notexpr
     @Override
     public String generate() {
         String childTemp = new stmtTranslator(parseTree.getChild(1)).generate();
         Temp temp = new Temp();
+        // assign expression's value into variable
         TranslationHandler.write(space + temp.toString() + " = NOT " + childTemp.toString());
         return temp.toString();
     }
