1465f33004cd1f8766717d3b79a5a0c52860456f
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 04:29:36 +0300

    Update FeatureTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/FeatureTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/FeatureTranslator.java
@@ -4,13 +4,17 @@ import Translation.Environment.ScopeHandler;
 import Translation.TranslateAndError.Translator;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+//((function| decl ))*
 public class FeatureTranslator extends Translator {
     ParseTree parseTree;
+    
+    //check that child of expression is feature
     public FeatureTranslator(ParseTree parsetree) {
         super(parsetree, COOLParser.FeatureContext.class);
         parseTree =parsetree;
     }
 
+    //generate 3address code for feature
     @Override
     public String generate() {
         ScopeHandler.pushScope();
@@ -18,10 +22,12 @@ public class FeatureTranslator extends Translator {
         for (int i = 0; i < parseTree.getChildCount(); ++i) {
 
             ParseTree child = parseTree.getChild(i);
+            //check is child is decl
             if (child instanceof COOLParser.DeclContext) {
                 String temp = new DeclTranslator(child).generate();
             }
 
+            //check is child is function
             if (child instanceof COOLParser.FunctionContext) {
                 String temp = new FunctionTranslator(child).generate();
             }
