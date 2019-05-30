3fbee5e5730801ae29999d8024455a20589c9130
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 04:35:47 +0300

    Update IsvoidTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/IsvoidTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/IsvoidTranslator.java
@@ -7,19 +7,24 @@ import Translation.TranslateAndError.Translator;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.stringtemplate.v4.ST;
 
+//ISVOID stmt
 public class IsvoidTranslator extends Translator {
     ParseTree parseTree;
 
+    //check that child of expression is isvoid
     public IsvoidTranslator(ParseTree parsetree) {
         super(parsetree, COOLParser.IsvoidExprContext.class);
         parseTree = parsetree;
     }
 
-
+    //generate 3address code for isvoid
     @Override
     public String generate() {
+        //call stmt class 
         String child = new stmtTranslator(parseTree.getChild(1)).generate();
         Temp myTemp = new Temp();
+       
+        // assign expression's value into variable
         TranslationHandler.write(space + myTemp.toString() + " = ISVOID " + child);
         return myTemp.toString();
 
