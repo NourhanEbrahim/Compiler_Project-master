552aac63d1d515af8d13a07290cbad834ca3c2a6
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 11:31:41 +0300

    Update MinusExprTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/MinusExprTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/MinusExprTranslator.java
@@ -7,17 +7,21 @@ import Translation.TranslateAndError.Translator;
 import gen.COOLParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+//MINUS stmt
 public class MinusExprTranslator extends Translator {
     ParseTree parseTree;
+    //ensure that the child of expression is minus
     public MinusExprTranslator(ParseTree parsetree) {
         super(parsetree, COOLParser.MinusExprContext.class);
         parseTree =parsetree;
     }
 
+    //generate 3address code for minus
     @Override
     public String generate() {
         String childTemp = new stmtTranslator(parseTree.getChild(1)).generate();
         Temp temp = new Temp();
+        // assign expression's value into variable
         TranslationHandler.write(space + temp.toString() + " = - " + childTemp.toString());
         return temp.toString();
     }
