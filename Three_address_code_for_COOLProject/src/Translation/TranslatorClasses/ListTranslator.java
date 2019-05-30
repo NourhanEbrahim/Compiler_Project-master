4710fbc64c20dee31ef17e8d35b3ebdd8c456f52
YaraEslam <36794384+YaraEslam@users.noreply.github.com>
2019-05-30 11:20:44 +0300

    Update ListTranslator.java

--- Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/ListTranslator.java
+++ Three_address_code_for_COOLProject/src/Translation/TranslatorClasses/ListTranslator.java
@@ -3,24 +3,30 @@ import gen.*;
 import Translation.TranslateAndError.Translator;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+//list ',' stmt | stmt
 public class ListTranslator extends Translator {
     ParseTree parseTree ;
+    
+    //check that the child of expression i list
     public ListTranslator(ParseTree parsetree) {
         super(parsetree, COOLParser.ListContext.class);
         parseTree = parsetree;
     }
 
+    //generate 3address code for list
     @Override
     public String generate() {
+        //check that it has one child
         if(parseTree.getChildCount() == 1){
+            //add the param counter
             param_num ++;
             String param = new stmtTranslator(parseTree.getChild(0)).generate();
             param_list.add(param);
 
-        }else{
-
+        }
+        //it has 3 children
+        else{
             new ListTranslator(parseTree.getChild(0)).generate();
-
             String param = new stmtTranslator(parseTree.getChild(2)).generate();
             param_num ++;
             param_list.add(param);
